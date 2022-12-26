6e3c7e9b351a91096e57b173fdb8e9a8c0b9278e
Ridwan Hanif <99933836+nickridwan@users.noreply.github.com>
2022-12-26 11:04:59 +0700

    Create dummy.dart

---
+++ dummy.dart
@@ -0,0 +1,20 @@
+{
+  "status": "",
+  "data": [
+    {
+      "username": "justin",
+      "email": "ridwanhanif@gmail.com",
+      "password": "crb123",
+    },
+    {
+      "username": "michelle",
+      "email": "ridwan@gmail.com",
+      "password": "crb321",
+    },
+    {
+      "username": "jordan",
+      "email": "hanif@gmail.com",
+      "password": "crb312",
+    }
+  ],
+};
